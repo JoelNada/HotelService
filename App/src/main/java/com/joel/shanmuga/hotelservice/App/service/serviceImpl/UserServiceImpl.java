14c94e23faabaefb277163e8ffb6193f01d73449
JoelNada20 <66004734+JoelNada@users.noreply.github.com>
2024-06-28 20:54:27 +0530

    utils, serviceimpl, helper and security with jwt

--- App/src/main/java/com/joel/shanmuga/hotelservice/App/service/serviceImpl/UserServiceImpl.java
+++ App/src/main/java/com/joel/shanmuga/hotelservice/App/service/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,12 @@
 package com.joel.shanmuga.hotelservice.App.service.serviceImpl;
 
 import com.joel.shanmuga.hotelservice.App.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+    @Override
+    public String registerUser() {
+        return "";
+    }
 }
